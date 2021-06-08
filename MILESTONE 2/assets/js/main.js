84b38c1042c2446e433e95d9991bf3205e37f5ec
dumi994 <dumi.9944@gmail.com>
2021-06-08 16:57:04 +0200

    non mi funziona la ricerca delle serie tv

---
+++ MILESTONE 2/assets/js/main.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: "#app",
+    data:{
+        uri:"https://api.themoviedb.org/3",
+        api_key:"3aa3ea13ba4c284ab4777842999b80d1",
+        films: " ",
+        query: " ",
+        series: " ",
+    },
+    methods:{
+        //FUNZIONE PER LA RICERCA DEL TITOLO DEL FILM
+        cercaFilm: function(){
+             axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.query}`)
+             .then (response =>{
+                 
+                 console.log(response.data.results);
+                 this.films = response.data.results;
+            }),
+            axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.query}`)
+            .then (resp =>{
+                
+                console.log(resp.data.results);
+                this.series = resp.data.results;
+           })
+
+        },
+       /*  cercaSerie: function(){
+
+        }
+        */
+    },
+    mounted(){
+     
+    }
+})
