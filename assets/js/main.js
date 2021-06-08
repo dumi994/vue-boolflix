3231d478c2b39995776962f0e87969cf12537a40
dumi994 <dumi.9944@gmail.com>
2021-06-08 16:21:36 +0200

    funzione non funziona 2

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+    el: "#app",
+    data:{
+        uri:"https://api.themoviedb.org/3",
+        api_key:"3aa3ea13ba4c284ab4777842999b80d1",
+        films: " ",
+        query: " ",
+    },
+    methods:{
+        cercaFilm: function(){
+             axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.query}`)
+             .then (response =>{
+                 
+                 console.log(response.data.results);
+                 this.films = this.response.data.results;
+            })
+
+        }
+       
+    },
+    mounted(){
+     
+    }
+})
