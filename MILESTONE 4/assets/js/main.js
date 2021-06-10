586464a0ae2f873b3ced4d9b80768ab181a1965c
dumi994 <dumi.9944@gmail.com>
2021-06-10 15:08:55 +0200

    perché non mi mostra la scritta al posto dell img rota?

---
+++ MILESTONE 4/assets/js/main.js
@@ -0,0 +1,41 @@
+const app = new Vue({
+    el: "#app",
+    data:{
+        uri:"https://api.themoviedb.org/3",
+        api_key:"3aa3ea13ba4c284ab4777842999b80d1",
+        films: "",
+        query: "",
+        series: "",
+        flagImg:'https://www.countryflags.io/',
+        error: null,
+        star: '<i class="far fa-star"></i>',
+    },
+    methods:{
+        //FUNZIONE PER LA RICERCA DEL TITOLO DEL FILM
+        cercaFilm: function(){
+             axios.get(`${this.uri}/search/movie?api_key=${this.api_key}&query=${this.query}`)
+             .then (response =>{
+                 /* console.log(response.data.results); */
+                 this.films = response.data.results;
+                //Errore quando la pagina non carica
+            }).catch(e =>{
+                this.error = "Ops, si è verificato un'errore con il server" + e
+                console.error(e);
+           })
+            //CERCA SERIE
+            axios.get(`${this.uri}/search/tv?api_key=${this.api_key}&query=${this.query}`)
+            .then (resp =>{
+               /*  console.log(resp.data.results); */
+                this.series = resp.data.results;  
+           //Errore quando la pagina non carica
+           }).catch(e =>{
+                this.error = "Ops, si è verificato un'errore con il server" + e
+                console.error(e);
+           })
+           
+        },
+    },
+    mounted(){
+        
+    }
+})
